13f12f134f5010ca9695fd8e9c1af4cf76dc3e0b
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-11-15 10:11:18 +0300

    update commit

--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 class AppColors {
   static const Color textColor = Color(0xFFccc7c5);
-  static const Color mainColor = Color(0xFF89dad0);
+  static const Color mainColor = Color.fromARGB(255, 30, 231, 208);
 
   static const Color iconColor1 = Color(0xFFffd28d);
   static const Color iconColor2 = Color(0xFFfcab88);
